913553e9a964b245bacabff7d254798e9d71833c
sinke237 <enowsinke@gmail.com>
2023-11-06 10:23:55 -0500

    sinke's portion

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ from flask import Flask, render_template
 
 
 
-year = db.column(db.integer)
-#king start copying from here
+@app.before_first_request
+def import data():
+    if Book.query.first() is None: # this only imports if table is empty
+        data = pd.read_csv('data.csv')
+        for row in data.itertuples():
+            db.session.add(Book(title=row.title, author=row.author, year=row.year))
+            db.session.commit()
 
+@app.route('/')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
 
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
