75bddea06d82bbc7dffbb26497147d9945cb75bc
Bansikah <tandapnoelbansikah@gmail.com>
2023-11-06 15:45:08 +0100

    create feature-delete-books

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+
+@app.before_first_request
+def import_data():
+    db.create_all()
+    if Book.query.first() is None:  # Only import if table is empty
+        data = pd.read_csv('data.csv')
+        for row in data.itertuples():
+            db.session.add(Book(title=row.title, author=row.author, year=row.year))
+        db.session.commit()
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
+
+@app.route('/delete/<int:id>')
+def delete_book(id):
+    book_to_delete = Book.query.get_or_404(id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect('/delete')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
 if __name__ == '__main__':
 	app.run(port=8080, debug=True)
