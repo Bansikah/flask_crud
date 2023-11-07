73edd7c15b247d3b4684d2190073e0ffb97b6395
nghaboris <borisngha6@gmail.com>
2023-11-07 09:40:46 +0100

    implementation of update book feature version1

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-#bryan start working from here 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
 
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(100))
+    year = db.Column(db.Integer)
 
+data_imported = False
 
+@app.before_request
+def import_data():
+    global data_imported
+    if not data_imported:
+        db.create_all()
+        if Book.query.first() is None:  # Only import if table is empty
+            data = pd.read_csv('data.csv')
+            for _, row in data.iterrows():
+                db.session.add(Book(title=row['title'],author=row['author'],year=row['year']))
+            db.session.commit()
+        data_imported = True
 
-
-
-
-
-year = db.column(db.integer)
-#king start copying from here
-
-
-
-
-
-db.session.commit()
-#caleb start here
 @app.route('/')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
 
-
-
-
-
-
-0'. port=8080
-#end
 
 
 
